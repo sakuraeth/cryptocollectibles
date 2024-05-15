cd1d3e6c404b4e9dd026f49064ca488b46958e59
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-05-15 14:03:23 +0300

    error catching

--- Collectible.sol
+++ Collectible.sol
@@ -7,25 +7,34 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract MyNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    mapping(uint256 => string) private _tokenURIs;
 
     constructor() ERC721("MyNFT", "MNFT") {}
 
     function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
+        require(recipient != address(0), "MyNFT: mint to the zero address");
+        
         _tokenIds.increment();
+        
         uint256 newItemId = _tokenIds.current();
+        
         _mint(recipient, newItemId);
+        
         _setTokenURI(newItemId, tokenURI);
+        
         return newItemId;
     }
 
     function transferNFT(address from, address to, uint256 tokenId) public {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "Caller is not owner nor approved");
+        require(to != address(0), "MyNFT: transfer to the zero address");
+        
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "MyNFT: caller is not owner nor approved");
+        
         _transfer(from, to, tokenId);
     }
 
     function getTokenDetails(uint256 tokenId) public view returns (string memory) {
-        require(_exists(tokenId), "Query for nonexistent token");
+        require(_exists(tokenId), "MyNFT: query for nonexistent token");
+        
         return tokenURI(tokenId);
     }
 }
