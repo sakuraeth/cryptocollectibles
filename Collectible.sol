5f5495d6a43dd9339fae9529290fa42a54b8c9ef
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-05-18 17:18:31 +0300

    improve error handling

--- Collectible.sol
+++ Collectible.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -11,30 +12,30 @@ contract MyNFT is ERC721URIStorage {
     constructor() ERC721("MyNFT", "MNFT") {}
 
     function mintToken(address recipient, string memory metadataURI) public returns (uint256) {
-        require(recipient != address(0), "MyNFT: mint to the zero address");
-        
+        require(recipient != address(0), "MyNFT: Cannot mint to the zero address");
+
         _totalMintedTokens.increment();
-        
+
         uint256 newTokenId = _totalMintedTokens.current();
-        
+
         _mint(recipient, newTokenId);
-        
+
         _setTokenURI(newTokenId, metadataURI);
-        
+
         return newTokenId;
     }
 
     function transferToken(address sender, address receiver, uint256 tokenId) public {
-        require(receiver != address(0), "MyNFT: transfer to the zero address");
-        
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "MyNFT: caller is not owner nor approved");
-        
+        require(receiver != address(0), "MyNFT: Cannot transfer to the zero address");
+
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "MyNFT: Caller is not owner nor approved for transfer");
+
         _transfer(sender, receiver, tokenId);
     }
 
     function fetchTokenMetadata(uint256 tokenId) public view returns (string memory) {
-        require(_exists(tokenId), "MyNFT: query for nonexistent token");
-        
+        require(_exists(tokenId), "MyNFT: Query for nonexistent token");
+
         return tokenURI(tokenId);
     }
 }
